8d28f61a592440924663df1d91a77ce6ef9cbb84
Gabriel Okello <gokello@munutech.com>
2018-05-21 10:28:09 +0300

    Changed Header

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html class="no-js">
     <head>
-        <title>SLMS</title>
+        <title>School Lab Monitoring System</title>
  		<link href="images/logo.png" rel="icon" type="image">
 		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
