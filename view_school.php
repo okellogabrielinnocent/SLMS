f0464c5a66e7cf55c41a12ac22b8e8d197e32edf
Gabriel Okello <gokello@munutech.com>
2018-05-21 12:49:43 +0300

    Rueben helped in identifying how to read all ISP for all the school

--- view_school.php
+++ view_school.php
@@ -13,26 +13,24 @@
                         <div  id="block_bg" class="block">
                             <div class="navbar navbar-inner block-header">
                                 <div class="muted pull-right">
-										<a href="schools.php"><i class="icon-arrow-left icon-large"></i> Back</a>
+									<a href="schools.php"><i class="icon-arrow-left icon-large"></i> Back</a>
 								</div>
                             </div>
                             <div class="block-content collapse in">
-							
-							
-							<div class="span6">
+                            <div class="span6">
 								<?php
 								$query = mysqli_query($connection,"select * from school_det where school_id = '$get_id'")or die(mysqli_error());
 								$row = mysqli_fetch_array($query);
 								
 								?>
 								<div class="alert alert-success">SCHOOL DETAILS</div>
+								<?php $prov=$row['isp_id'];?>
 								Name: <strong><?php echo $row['school_name']; ?></strong><hr>
 								Contact 1: <strong><?php echo $row['contact1']; ?></strong><hr>
 								Contact 2: <strong><?php echo $row['contact2']; ?></strong><hr>
 								Address: <strong><?php echo $row['location']; ?></strong><hr>
 								Type: <strong><?php echo $row['school_type']; ?></strong><hr>
-								IP Adress: <strong><?php echo $row['pub_ip_add']; ?></strong><hr>
-								ISP Provider: <strong><?php echo $row['isp_id']; ?></strong><hr>
+								Public IP Adress: <strong><?php echo $row['pub_ip_add']; ?></strong><hr>								
 							</div>
 							<div class="span5">
 								<?php
@@ -63,23 +61,19 @@
 								</thead>
 								<tbody>
 								<?php
-								$query3 = mysqli_query($connection,"select * from isp_det where isp_id = '$get_id'")or die(mysqli_error());
+								$query3 = mysqli_query($connection,"select * from isp_det where isp_id = '$prov'")or die(mysqli_error());
 								$row = mysqli_fetch_array($query3);
 								?>
 								<tr>
-								<td><?php echo $row['name']; ?></td> 
+								<td><?php echo $isp = $row['name'];?></td> 
 								<td><?php echo $row['ip_address']; ?></td> 
 								<td><?php echo $row['location']; ?></td> 
-								<td><?php echo $row['contact']; ?></td> 
-								
+								<td><?php echo $row['contact']; ?></td>								
 								</tr>
 								</tbody>
 								</table>
-
 							</div>
-							
-
-                            </div>
+							</div>
                         </div>
                         <!-- /block -->
                     </div>
